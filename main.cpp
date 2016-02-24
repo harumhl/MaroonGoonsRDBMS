6194c57fc3b41251cfe1a0fb90b2ca1b39cb6e52
haru-mhl <haru.mhl@tamu.edu>
2016-02-24 13:56:43 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Engine.h"
+#include "League.h"
+using namespace std;
+
+int main() {
+  Engine* engine = new Engine();
+  Parser* parser = new Parser();
+  
+  while (true) {
+    string line;
+    getline(line, cin);
+   
+    // parsing
+   
+    // executer
+  }
+}
