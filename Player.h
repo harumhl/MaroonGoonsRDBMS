35259fe72d993b412c59fbe4ddc186adb731962a
haru-mhl <haru.mhl@tamu.edu>
2016-02-24 13:42:35 -0600

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Player {
+  public:
+    Player() {}
+    
+  private:
+    string name;
+    int    age;
+    string teamname;
+    string position;
+    // how are we going to keep different sport player's personal stats?
+    // i.e. football: rush yard, baseball: hits, etc.
+}
