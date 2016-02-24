e29b0c70d2178c5e69979b84d3e9a5d74171a593
haru-mhl <haru.mhl@tamu.edu>
2016-02-24 13:38:52 -0600

    Create Team.h

---
+++ Team.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include "Player.h"
+using namespace std;
+
+class Team {
+  public:
+    Team() {}
+    
+    void setPlayers (vector<Player*> ps) { players = ps; }
+    vector<Player*> getPlayers() { return players; }
+    Player* getPlayer(int i) { return players.at(i); }
+    
+    void addPlayer (Player* p) { players.push_back(p); }
+    
+  private:
+    vector<Player*> players;
+    int rank;
+    // team's wins and losses
+}
