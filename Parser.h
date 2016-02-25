97a550db05161997338cad7be5a81d8a79a16222
haru-mhl <haru.mhl@tamu.edu>
2016-02-25 10:18:06 -0600

    for Parser - Copied from P1D2,3

---
+++ Parser.h
@@ -0,0 +1,41 @@
+// The parser will have a function called Parse() that will return a value of type Token(token.h). Token.h will essentially just be
+// a value type that will have private values we assign. It really shouldn't have any functions only members.
+#ifndef PARSER_H_
+#define PARSER_H_
+
+#include "Token.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class Parser{
+  public:
+    Parser() {currentIndex = 0;}
+    Parser(string query) : query(query) {currentIndex = 0;}
+    vector<Token> getTokens(){return tokens;}
+    string getQuery(){ return query; }
+    vector<Token> splitInput(string query, vector<int>& conditions);
+    Token recognizeToken(string input);
+    string skipChars(int distance);
+    vector< vector<Token> > getTokenVector(){ return vtokens; }
+    void tokenVector(vector<Token> tokens);
+
+    
+  private:
+    string query;
+    vector< vector<Token> > vtokens;
+    int currentIndex;
+    string::iterator iter;
+    vector<Token> tokens;
+    string symbols1[12] = {"<", ">", "{", "}", "+", "-", "*", ";", "(", ")", ",", "="};
+    string symbols2[7] = {"==", "!=", "<=", ">=", "<-", "||", "&&"};
+    
+    void setToken();
+    void increment();
+    string toUpper(string str);
+
+  
+};
+
+#endif
