bc73465c6632575e57d34bd82bd61eb698acacf3
haru-mhl <haru.mhl@tamu.edu>
2016-02-25 10:19:48 -0600

    for Engine - Copied from P1D2,3

---
+++ Tuple.h
@@ -0,0 +1,22 @@
+// COPIED EXACTLY FROM P1D2 on 2/16
+
+#ifndef TUPLE_H_
+#define TUPLE_H_
+#include <string> 
+#include <vector>
+using namespace std;
+
+struct Tuple {
+  public:
+    Tuple() {}
+    Tuple(vector<string> newContents): contents(newContents) {}
+    vector<string> getContents () { return contents; }
+    string dataPoint(int index) { return contents.at(index); }
+    void setContents(vector<string> newContents) {contents = newContents;}
+    void addData(string newData) {contents.push_back(newData);}
+    void updateValue(int index, string newData) {contents[index]= newData;}
+  
+  private:
+    vector<string> contents; // contents of each row
+};
+#endif
