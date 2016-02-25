4d51d33eca1dd14f074b68879b20b85b0859b570
haru-mhl <haru.mhl@tamu.edu>
2016-02-25 10:16:08 -0600

    for Engine - Copied from P1D2,3

---
+++ Engine.h
@@ -0,0 +1,65 @@
+#ifndef ENGINE_H_
+#define ENGINE_H_
+#include <iostream>
+#include <string>
+#include "Relation.h"
+#include "Tree.h"
+#include "TreeNode.h"
+#include "Condition.h"
+using namespace std;
+
+class Engine {
+   public:
+    Engine() {}
+    void open(string filename);
+    void close(string relation);
+    void write(string relation, string filename = "Output.txt");
+    void exit_(); //.
+    void show(string relation);
+    void show(Relation* the_relation);
+    
+    void createRelation(string relation, vector<Attribute*> attributes);
+    void createRelation(Relation* relation);
+    void dropRelation(string relation);
+    void renameRelation(string relation, string new_name);
+    
+    void renameAttribute(string relation, int attribute_index, string new_name);
+    
+    void insertTuple(string relation, Tuple* tuple);
+    void deleteTuple(string relation, int tuple_index);// WHAT SHOULD BE THE SECOND ARG?
+    void update(Relation* relation, int attributeIndex, Tuple* tuple, string newData);
+    
+    Relation* select(string relation, vector<string> attributeName, Tree* tree);
+    Relation* project(string relation, vector<Attribute*> attributeNames);
+    Relation* union_(string relation1, string relation2);
+    Relation* difference(string relation1, string relation2);
+    Relation* crossProduct(string relation1, string relation2);
+    Relation* naturalJoin(string relation1, string relation2);
+    
+    
+    //temp
+    Relation* getRelation(int i) { return relations.at(i); }
+    void changeRelationName (int relation_index, string relation);
+    int numRelations = 0;
+    string relationName(int index);
+    bool tupleExists(Relation* relation, Tuple* tuple);
+    vector<pair<int,int>> attributesInBoth(string relation1, string relation2);
+    vector<pair<int,int>> tuplesInBoth(string relation1, string relation2, string searchData);
+    int findRelation (string relation); // returns the index in relations
+    
+ private:
+    vector<Relation*> relations;
+    vector<Relation*> tempRelations; //The relations that will not be saved(views from algebra operations)
+    
+    vector<string>  tokenize (string input, char delimeter);
+    pair<int,int>   findSameAttribute (string relation1, string relation2); // if found, return index of relation1, or else -1
+    string          trimString (string str);
+    string          toUpper (string str); // for Type type
+    bool            isUnionCompatible (string relation1, string relation2);
+    bool            testCondition (string relation, Tuple* tuple, Condition* condition);
+
+    vector<Attribute*> findAttributes(string relation, vector<string> attributeNames, vector<int>& attribute_indices);
+
+};
+
+#endif
