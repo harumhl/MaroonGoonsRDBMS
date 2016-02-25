083faa9bcfbb19d1b583a9fca6cb16e7cab276ad
haru-mhl <haru.mhl@tamu.edu>
2016-02-25 10:17:24 -0600

    for Parser - Copied from P1D2,3

---
+++ Executer.h
@@ -0,0 +1,39 @@
+#ifndef EXECUTE_H_
+#define EXECUTE_H_
+
+#include <vector>
+#include "Engine.h"
+#include "Token.h"
+#include "Parser.h"
+using namespace std;
+
+class Executer {
+  public:
+	Executer() {}
+    void execute(Engine* engine, vector<Token> workToDo);
+    void start();
+    void program();
+    void command();
+    void query();
+    Relation* expr();
+    Relation* atomicExpr();
+    Relation* rename();
+    Relation* project();
+    Relation* combine(Relation* relation);
+    vector<Attribute*> getAttributeList();
+    void expect(Token::TokenTypes type);
+    void nextToken();
+    void setToken();
+    void insert();
+    bool lookAhead(Token::TokenTypes type);
+    bool atEnd();//returns true if the current token is the last
+    
+    vector<Token> tokens;
+    Token token;
+    int currentIndex;
+  private:
+    Engine* engine;
+
+};
+
+#endif
