551cade59d35d7b1fab0508b078237bd4a37e20e
am56472 <am56472@tamu.edu>
2016-02-25 18:40:40 -0600

    Testing for project() and rename()

--- main.cpp
+++ main.cpp
@@ -33,3 +33,42 @@ void help() {
     cout << "\"open <filename w/ extension>\"" << " to open a relation" << endl;
     cout << "\"show <relation name>\"" << " to display a relation" << endl;
 }
+
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include "Parser.h"
+#include "Token.h"
+#include "Executer.h"
+#include "Engine.h"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+/*
+ *
+
+using namespace std;
+int main(){
+
+	Engine* engine = new Engine();
+    Parser* parse1 = new Parser();
+    string q1 = "a <- Project (Animal, Number) Students;";
+    string q2 = "a <- rename (Animal, Species) Students";
+    engine->open("input2.txt");
+    engine->changeRelationName(0, "Students");
+    engine->show("Students");
+    vector<int> theVec;
+
+    vector<Token> tokenvec = parse1->splitInput(q2, theVec);
+    /*for(int i = 0; i < tokenvec.size(); i++){
+        cout << tokenvec.at(i).getValue() << "/t " << tokenvec.at(i).getTokenStr(tokenvec.at(i).getTokenType())<< endl;
+    }
+
+    Executer* exec = new Executer();
+    exec->execute(engine, tokenvec);
+    engine->show("Students");
+    //engine->show("projection");
+}*/
