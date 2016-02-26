63bc605978bcb676d40749aceec2b21f247dbc38
haru-mhl <haru.mhl@tamu.edu>
2016-02-25 18:01:25 -0600

    for Engine - Copied from P1D2,3

---
+++ Tree.h
@@ -0,0 +1,32 @@
+#ifndef TREE_H_
+#define TREE_H_
+
+#include <iostream>
+#include "TreeNode.h"
+#include "Condition.h"
+#include "Tuple.h"
+#include "Attribute.h"
+#include "Token.h"
+#include "Parser.h"
+using namespace std;
+
+class Tree {
+    public:
+    Tree() {}
+  
+    bool isNodeLeaf(TreeNode* node);
+    TreeNode* buildTree(vector<Token> tokenVec, int& starting);
+    TreeNode* condition(vector<Token> tokenVec, int& starting);
+    TreeNode* conjunction(vector<Token> tokenVec, int& starting);
+    TreeNode* comparison(vector<Token> tokenVec, int& starting);
+    
+    bool evalCond(Tuple* tuple, Condition* condition, vector<Attribute*> attributes);
+    bool evalTree(Tuple* tuple, vector<Attribute*> atts, TreeNode* parent = NULL);
+    
+    string toUpper (string str);
+    
+    private:
+    TreeNode* the_root;
+};
+
+#endif
