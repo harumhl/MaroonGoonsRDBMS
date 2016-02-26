1e728557a109c8ad0cf195d6803aaa173331746b
harumhl <haru.mhl@tamu.edu>
2016-02-25 18:14:03 -0600

    added help() for main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "League.h"
 using namespace std;
 
+void help();
 int main() {
   Engine* engine = new Engine();
   Parser* parser = new Parser();
@@ -13,10 +14,15 @@ int main() {
     string line;
     getline(cin, line);
    
-      if (line == "q" || line == "quit") return 0;
+      // temp
+      if      (line == "q" || line == "quit") return 0;
+      else if (line == "h" || line == "help") help();
       
     // parsing
    
     // executer
   }
+}
+void help() {
+    
 }
