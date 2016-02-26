32916f85fbbb674df2f5a42d6b94159bf681c349
haru-mhl <haru.mhl@tamu.edu>
2016-02-25 18:02:31 -0600

    #ifndef, #define, #endif

--- Application.h
+++ Application.h
@@ -1,7 +1,11 @@
 // Base class for League, Team, and Player class
 // So Tuple objects can point to any of them as necessary
 
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
 class Application {
   public:
     Application() {}
 };
+#endif
