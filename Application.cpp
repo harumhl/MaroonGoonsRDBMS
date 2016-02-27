1469acc38c74a2923ae11c7345a43249b0132f3c
kripa219 <kripa219@email.tamu.edu>
2016-02-27 16:58:06 -0600

    fixed mainMenu()

--- Application.cpp
+++ Application.cpp
@@ -20,18 +20,18 @@ void Application::mainMenu()
 	cout << "1) View Leagues" << endl;
 	cout << "2) View Teams in League" << endl;
 	cout << "3) View Players in a Team" << endl;
-	cout << "4) Add League" << endl;
-	cout << "5) Add Team" << endl;
-	cout << "6) Add Players" << endl;
+	cout << "4) Add League(s)" << endl;
+	cout << "5) Add Team(s)" << endl;
+	cout << "6) Add Players(s)" << endl;
 
 
 	cin >> user_choice;
 	cout << endl;
 
-	/*if(user_choice == 1) 
-	if(user_choice == 2)
-	if(user_choice == 3)
-	if(user_choice == 4)
-	if(user_choice == 5)
-	if(user_choice == 6)*/
+	if(user_choice == 1) viewLeagues();
+	if(user_choice == 2) viewTeams();
+	if(user_choice == 3) viewPlayers();
+	if(user_choice == 4) addLeague();
+	if(user_choice == 5) addTeam();
+	if(user_choice == 6) addPlayer();
 }
